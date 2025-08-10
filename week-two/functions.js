5c5ba1ffbc80fb27160bf64a3e9e78d05a4064be
Becky-234 <kiraborebecca04@gmail.com>
2025-08-10 16:50:32 +0300

    conditionals.js

---
+++ week-two/functions.js
@@ -0,0 +1,14 @@
+//Functions
+//  Is a block of code designed to perform a specific task.
+
+let fridge = {
+    name: "sumsang",
+    size: "large",
+    height: 100,
+    freeze: function(){
+        return this.name + ' ice'
+    },
+};
+ console.log(fridge.freeze());
+
+ 
---
+++ week-two/strings.js
@@ -0,0 +1,34 @@
+//strings are 4 storing text
+//N.B: Strings are written in quotation marks
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"'; 
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
+
+//length of a string
+// let sentence = "My name is Kirabo Rebecca";
+// console.log(sentence);
+// let length = text.length;
+// console.log(length);
+
+//escape characters 
+let text = "We are the so-called \"Vikings\" from the north.";
+let intro = "I\'m Kirabo Rebecca \"Becky\" from Uganda.";
+console.log(text);
+console.log(intro);
+
+/*
+Code	     Result
+\b	         Backspace
+\f	         Form Feed
+\n	         New Line
+\r	         Carriage Return
+\t	         Horizontal Tabulator
+\v	         Vertical Tabulator
+
+*/
+
+
+//Breaking long lines: Is dona after an operator.
