a2e5a115e0d1869ca9f89fb18d0340e1baf51dbc
Becky-234 <kiraborebecca04@gmail.com>
2025-08-13 17:09:13 +0300

    coaching

--- week-three/event.js
+++ week-three/event.js
@@ -32,16 +32,19 @@ const furniture = document.getElementById("furniture");
 const customer = document.getElementById("customer");
 const total = document.getElementById("total");
 const calcBtn = document.getElementById("calcBtn");
+const placeOrder = document.getElementById("orderBtn");
 
 function updateTotal() {
     const price = Number(document.getElementById("price").value);
     const qty = Number(document.getElementById("qty").value);
-
+     
     const cost = price * qty;
     total.textContent = `Total: UGX ${cost.toLocaleString()}`;
 }
 
 qty.addEventListener("input", updateTotal);
 price.addEventListener("input", updateTotal);
+placeOrder.addEventListener("mouseover", updateTotal);
+
 
 
