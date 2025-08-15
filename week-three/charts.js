f972a149e4f2519ec000ce3d605a7f52222e9c32
Becky-234 <kiraborebecca04@gmail.com>
2025-08-15 06:56:47 +0300

    syntax for charts

--- week-three/charts.js
+++ week-three/charts.js
@@ -1,22 +1,27 @@
- 
-// const ctx = document.getElementById('myChart').getContext('2d');
-// const myChart = new myChart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       backgroundColor: 'rgba(54, 162, 235, 0.2)',
-//       borderColor: 'rgba(54, 162, 235, 1)',
-//       borderWidth: 1
-//     }]
-//   }
+ //Syntax for a bar chart
+// const myChart = new Chart("myChart", {
+//   type: "bar",
+//   data: {},
+//   options: {}
+// });
+
+//Syntax for a line chart
+// const myChart = new Chart("myChart", {
+//   type: "line",
+//   data: {},
+//   options: {}
+// });
+
+//Syntax for a scatter chart
+// const myChart = new Chart("myChart", {
+//   type: "scatter",
+//   data: {},
+//   options: {}
 // });
 
 
 const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
+const myChart = new myChart(ctx, {
   type: 'bar',
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
