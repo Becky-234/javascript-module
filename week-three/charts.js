41ed7211649dc29cd326b491fc2982dddeaba683
Becky-234 <kiraborebecca04@gmail.com>
2025-08-19 15:39:38 +0300

    error handling

--- week-three/charts.js
+++ week-three/charts.js
@@ -1,4 +1,9 @@
- //Syntax for a bar chart
+/*
+Chart.j.s
+Is a popular open-source j.s library for creating responsive and iteractive data visualisations in wed applications.
+*/
+
+//Syntax for a bar chart
 // const myChart = new Chart("myChart", {
 //   type: "bar",
 //   data: {},
@@ -19,18 +24,40 @@
 //   options: {}
 // });
 
-
 const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new myChart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgba(54, 162, 235, 1)',
-      borderWidth: 1
-    }]
-  }
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
